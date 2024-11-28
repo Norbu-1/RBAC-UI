d534d2b2eb24f44b474deb857522d3f86f93f37a
Tsewang Norbu <153228252+Norbu-1@users.noreply.github.com>
2024-11-28 09:06:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import Navbar from "./Dashboard/Navbar"
-import Footer from "./Dashboard/footer"
+import Footer from "./Dashboard/Footer"
 import HomeDashboard from "./Admin/HomeDashboard"
 function App() {
  const [logOut,setLogOut]=useState()
